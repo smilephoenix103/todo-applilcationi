fb980fb5cef58cca42fc6fc8ab54d13e853b7a10
smilephoenix103 <marugotomoaki@gmail.com>
2022-05-13 17:02:12 +0900

    complete spinner before loading...

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -3,16 +3,19 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import axios from "axios"
 
+
 import TodoItems from "./TodoItems";
 import TodoItem from "./TodoItem";
 import TodoForm from "./TodoForm";
+import Spinner from "./Spinner";
 
 class TodoApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            todoItems:[],
-            hideCompletedTodoItems: false
+            todoItems: [],
+            hideCompletedTodoItems: false,
+            isLoading: true
         };
         this.getTodoItems = this.getTodoItems.bind(this);
         this.createTodoItem = this.createTodoItem.bind(this);
@@ -27,17 +30,20 @@ class TodoApp extends React.Component {
         axios
             .get("/api/v1/todo_items")
             .then(response => {
+                this.setState({isLoading: true});
                 const todoItems = response.data;
-                this.setState({ todoItems });
+                this.setState({todoItems});
+                this.setState({isLoading: false});
             })
             .catch(error => {
+                this.setState({isLoading: true});
                 console.log(error);
             });
     }
 
     createTodoItem(todoItem) {
         const todoItems = [todoItem, ...this.state.todoItems];
-        this.setState({ todoItems });
+        this.setState({todoItems});
     }
 
     toggleCompletedTodoItems() {
@@ -45,23 +51,29 @@ class TodoApp extends React.Component {
             hideCompletedTodoItems: !this.state.hideCompletedTodoItems
         });
     }
+
     render() {
         return (
             <>
-                <TodoForm createTodoItem={this.createTodoItem}/>
-                <TodoItems
-                    toggleCompletedTodoItems={this.toggleCompletedTodoItems}
-                    hideCompletedTodoItems={this.state.hideCompletedTodoItems}
-                >
-                    {this.state.todoItems.map(todoItem => (
-                        <TodoItem
-                            key={todoItem.id}
-                            todoItem={todoItem}
-                            getTodoItems={this.getTodoItems}
+                {!this.state.isLoading && (
+                    <>
+                        <TodoForm createTodoItem={this.createTodoItem}/>
+                        <TodoItems
+                            toggleCompletedTodoItems={this.toggleCompletedTodoItems}
                             hideCompletedTodoItems={this.state.hideCompletedTodoItems}
-                        />
-                    ))}
-                </TodoItems>
+                        >
+                            {this.state.todoItems.map(todoItem => (
+                                <TodoItem
+                                    key={todoItem.id}
+                                    todoItem={todoItem}
+                                    getTodoItems={this.getTodoItems}
+                                    hideCompletedTodoItems={this.state.hideCompletedTodoItems}
+                                />
+                            ))}
+                        </TodoItems>
+                    </>
+                )}
+                {this.state.isLoading && <Spinner/>}
             </>
         )
     }
@@ -70,5 +82,5 @@ class TodoApp extends React.Component {
 document.addEventListener('turbolinks:load', () => {
     const container = document.getElementById('todo-app')
     const root = createRoot(container)
-    root.render(<TodoApp />)
+    root.render(<TodoApp/>)
 })
